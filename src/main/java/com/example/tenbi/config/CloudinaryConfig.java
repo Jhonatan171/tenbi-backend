31b1bc100929492b73b15981d508e4de6103b8f5
Jhonatan171 <jhoanatanguevaravasquez@gmail.com>
2025-12-09 18:18:03 -0500

    Create CloudinaryConfig.java

---
+++ src/main/java/com/example/tenbi/config/CloudinaryConfig.java
@@ -0,0 +1,37 @@
+package com.example.tenbi.config;
+
+// Archivo: CloudinaryConfig.java
+
+import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class CloudinaryConfig {
+
+    // Las variables son leídas automáticamente de Render
+    @Value("${CLOUDINARY_CLOUD_NAME}")
+    private String cloudName;
+
+    @Value("${CLOUDINARY_API_KEY}")
+    private String apiKey;
+
+    @Value("${CLOUDINARY_API_SECRET}")
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        // Mapea las claves de Cloudinary a sus valores
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
+
+        // Inicializa y retorna el objeto Cloudinary
+        return new Cloudinary(config);
+    }
+}
